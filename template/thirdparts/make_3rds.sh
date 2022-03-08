42f0b412c89da9075f09249f65a5349232b395c6
Victor <drjone@gmail.com>
2022-03-08 17:38:27 +0800

    modify

--- template/thirdparts/make_3rds.sh
+++ template/thirdparts/make_3rds.sh
@@ -9,24 +9,11 @@ RUNDIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
 ROOTDIR="$( cd -P "$( dirname "$RUNDIR/.././" )" >/dev/null 2>&1 && pwd )"
 echo "This is stand CMake develop project tools"
 echo "script run at ${RUNDIR}"
-echo "project root at ${ROOTDIR}"
-cd "${ROOTDIR}" || exit
 
-
-echo "./3rds all 3rd part components src and lib"
-echo "./thirdparts 3rd part component build tools"
-
-mkdir -p src
-mkdir -p doc
-mkdir -p tools
-mkdir -p 3rds/lib
-mkdir -p 3rds/bin
-mkdir -p 3rds/include
-mkdir -p 3rds/src
-mkdir -p thirdparts/tmp
-mkdir -p thirdparts/patches
-mkdir -p deploy/tools
-mkdir -p test
-
-cp -R "${RUNDIR}"/template/thirdparts/*.*  "${ROOTDIR}"/thirdparts/
-cp -R "${RUNDIR}"/template/thirdparts/patches/*.*  "${ROOTDIR}"/thirdparts/patches/
+echo "!!!!!!!!!!!!!!! Build 3rd Parts !!!!!!!!!!!!!!!"
+echo "!!!!!!!!!!!!!!! 3rd Parts Build Type :${1}!!!!!!!!!!!!!!!"
+mkdir "$RUNDIR"/tmp
+cd "$RUNDIR"/tmp
+pwd
+cmake  -DCMAKE_BUILD_TYPE=${1} ..
+make all
